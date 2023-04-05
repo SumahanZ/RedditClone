e052931147fc97483d7ac60fe87729cf964c2414
SumahanZ <83936130+SumahanZ@users.noreply.github.com>
2023-04-06 01:34:23 +0700

    s

--- lib/features/community/controller/community_controller.dart
+++ lib/features/community/controller/community_controller.dart
@@ -1,14 +1,21 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:reddit_app/core/constants/constants.dart';
+import 'package:reddit_app/core/providers/storage_repository_provider.dart';
 import 'package:reddit_app/core/utils.dart';
 import 'package:reddit_app/features/auth/controller/auth_controller.dart';
 import 'package:reddit_app/features/community/repository/community_repository.dart';
 import 'package:reddit_app/models/community_model.dart';
 import 'package:routemaster/routemaster.dart';
 
-final communityControllerProvider = StateNotifierProvider<CommunityController, bool>((ref) {
-  return CommunityController(communityRepository: ref.watch(communityRepositoryProvider), ref: ref);
+final communityControllerProvider =
+    StateNotifierProvider<CommunityController, bool>((ref) {
+  return CommunityController(
+      storageRepository: ref.watch(storageRepositoryProvider),
+      communityRepository: ref.watch(communityRepositoryProvider),
+      ref: ref);
 });
 
 final userCommunitiesProvider = StreamProvider((ref) {
@@ -24,10 +31,15 @@ final getCommunityByNameProvider = StreamProvider.family((ref, String name) {
 class CommunityController extends StateNotifier<bool> {
   final CommunityRepository _communityRepository;
   final Ref _ref;
+  final StorageRepository _storageRepository;
   CommunityController(
-      {required CommunityRepository communityRepository, required Ref ref})
+      {required CommunityRepository communityRepository,
+      required Ref ref,
+      required StorageRepository storageRepository})
       : _communityRepository = communityRepository,
-        _ref = ref, super(false);
+        _ref = ref,
+        _storageRepository = storageRepository,
+        super(false);
 
   void createCommunity(String name, BuildContext context) async {
     state = true;
@@ -39,7 +51,8 @@ class CommunityController extends StateNotifier<bool> {
         avatar: Constants.avatarDefault,
         members: [uid],
         mods: [uid]);
-    final createdResponse = await _communityRepository.createCommunity(community);
+    final createdResponse =
+        await _communityRepository.createCommunity(community);
     state = false;
     createdResponse.fold((error) => showSnackBar(context, error.message), (r) {
       showSnackBar(context, "Community  created successfully!");
@@ -54,5 +67,19 @@ class CommunityController extends StateNotifier<bool> {
 
   Stream<Community> getCommunityByName(String name) {
     return _communityRepository.getCommunityByName(name);
+  }
+
+  void editCommunity(
+      {required File? profileFile,
+      required File? bannerProfile,
+      required BuildContext context,
+      required Community community}) async {
+    if (profileFile != null) {
+      //communities/profile/memes (image saved in the path in the left)
+      final res = await _storageRepository.storeFile(
+          path: "communities/profile", id: community.name, file: profileFile);
+      res.fold((l) => showSnackBar(context, l.message),
+          (r) => community = community.copyWith(avatar: r));
+    }
   }
 }
